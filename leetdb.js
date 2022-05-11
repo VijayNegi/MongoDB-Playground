3c4c334450859c03cea98f0da31d6e41bc628134
VIJAY SINGH NEGI <vinegi@adobe.com>
2022-05-11 12:47:32 +0530

    scripts for leetcode

--- leetdb.js
+++ leetdb.js
@@ -8,19 +8,16 @@ async function connect() {
 		await client.connect()
 		const db = client.db("leetdb")
 		console.log(`connected to database ${db.databaseName}`)
-		const slugMap = db.collection("id-slug-map")
-		const response  = await fetchAllProblems()
-		let total  = response.data.problemsetQuestionList.total
-		let problemList = response.data.problemsetQuestionList.questions
+		const slugMap = db.collection("slugMap")
+		// const response  = await fetchAllProblems()
+		// let total  = response.data.problemsetQuestionList.total
+		// let problemList = response.data.problemsetQuestionList.questions
 
-		const insertManyresult = await slugMap.insertMany(problemList);
-		let ids = insertManyresult.insertedIds;
-   		console.log(`${insertManyresult.insertedCount} documents were inserted.`);
-		// problemList.forEach(async (p) => {
-		// 	const insert = await slugMap.insertOne(p)
-		// 	console.log(`inserted problem = ${insert.insertedId}`)
-		// })
-		
+		// const insertManyresult = await slugMap.insertMany(problemList);
+		// let ids = insertManyresult.insertedIds;
+   		// console.log(`${insertManyresult.insertedCount} documents were inserted.`);
+		const response  = await fetchProblemData("combination-sum-iii")
+		console.log(response)
 	}
 	catch(ex){
 		console.error(`Something bad happend ${ex}`)
--- leetfetch.js
+++ leetfetch.js
@@ -30,7 +30,11 @@ const fetchProblemData = async (questionSlug) => {
 
 	const init = {
 		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
+		headers: { 'Content-Type': 'application/json',
+					'origin': 'https://leetcode.com',
+					'referer': 'https://leetcode.com/',
+					'cookie': 'cf_clearance=b6133f557944682b880aff12df9048bbd7de88af-1624349454-0-150; _ga=GA1.2.2145902584.1635679193; gr_user_id=6d40fe7b-a2c8-4338-9c69-fb4102dfd7c8; NEW_PROBLEMLIST_PAGE=1; __stripe_mid=6b19c0b4-e9c8-4c0e-b01f-70896500d12f6b6027; 87b5a3c3f1a55520_gr_last_sent_cs1=n361; csrftoken=1w28872a2alH1SyxpObLxuKSwcuDPkXibghXTX6T2Kl79jxackNSp4HR0ddyBlsi; __atuvc=17|13,23|14,4|15,4|16,4|17; 87b5a3c3f1a55520_gr_cs1=n361; LEETCODE_SESSION=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfYXV0aF91c2VyX2lkIjoiMjgzNDYxOCIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImFsbGF1dGguYWNjb3VudC5hdXRoX2JhY2tlbmRzLkF1dGhlbnRpY2F0aW9uQmFja2VuZCIsIl9hdXRoX3VzZXJfaGFzaCI6Ijc4NDdhZTk5Y2ViOGYyZjcwNjI3ZTJjNjBhMjBkNDllNTU4YzBiZGYiLCJpZCI6MjgzNDYxOCwiZW1haWwiOiJhc3Ryby52aWpheUBnbWFpbC5jb20iLCJ1c2VybmFtZSI6Im4zNjEiLCJ1c2VyX3NsdWciOiJuMzYxIiwiYXZhdGFyIjoiaHR0cHM6Ly9hc3NldHMubGVldGNvZGUuY29tL3VzZXJzL2F2YXRhcnMvYXZhdGFyXzE2NDk1MTM4NTQucG5nIiwicmVmcmVzaGVkX2F0IjoxNjUyMTE4ODA3LCJpcCI6IjE2My41My44Ni4xODQiLCJpZGVudGl0eSI6IjkxOWIzY2MzZGRlODgxNzU4YzRhYTJjYmFmNjJmN2Y2IiwiX3Nlc3Npb25fZXhwaXJ5IjoxMjA5NjAwLCJzZXNzaW9uX2lkIjoxNzk3NTA2NSwiY29udmVyc2lvbl90YXJnZXRzIjp7IlJ4dGFWeEVLQVFSSEdVVlFTaGdYRlFNVVRCUUdBQndFRFF0QkZGZFhTbDhhSFZKUUZoc1hBZz09Ijp7InNlbmRfc2Vzc2lvbl9pZHMiOlsxODU3XSwiZW1haWwiOiJhc3Ryby52aWpheUBnbWFpbC5jb20ifSwiUnh0YVZ4RUtBUVJIRDFCY0RnTUxYUXNYVzAwQUhBWmRXa0FORmc9PSI6eyJzZW5kX3Nlc3Npb25faWRzIjpbMTg1N10sImVtYWlsIjoiYXN0cm8udmlqYXlAZ21haWwuY29tIn19fQ.CqRJE8ZBEcA9H5ox9bhQmh4Y6XJO06RzpXIiuCG3Z8c; c_a_u="bjM2MQ==:1noJFs:RY1Vr85Gh75HYa5ItOHSp_R5EmE"'
+		 },
 		body: JSON.stringify({  operationName: "questionData",
 							 	variables: { titleSlug: questionSlug, },
 								query: QUESTION_DATA_QUERY }),
